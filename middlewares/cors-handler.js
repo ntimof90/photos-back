8083014ba9a9fcdf977bbd584f312391a2d85489
NTimofeev <nt-90@yandex.ru>
2025-02-03 00:03:08 +0300

    fix: cors

--- middlewares/cors-handler.js
+++ middlewares/cors-handler.js
@@ -3,11 +3,12 @@ const { allowedCors, allowedMethods } = require('../config');
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.set('Access-Control-Allow-Origin', origin);
+    res.set('access-control-allow-origin', origin);
   }
   if (req.method === 'OPTIONS') {
-    res.set('Access-Control-Allow-Methods', allowedMethods);
-    res.set('Access-Control-Allow-Headers', req.headers['Access-Control-Request-Headers']);
+    res.set('access-control-allow-methods', allowedMethods);
+    console.log(allowedMethods);
+    res.set('access-control-allow-headers', req.headers['access-control-request-headers']);
     res.end();
     return;
   }
